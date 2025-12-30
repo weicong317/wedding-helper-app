12d7f3f485a6a8c6b0fd1ffa7f41e38465256d62
weicong317 <weicong317@gmail.com>
2025-12-30 18:44:54 +0800

    refactor: 💡 add in loader for images

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 import ImageViewer from "@/components/ImageViewer";
 import ScrollToTop from "@/components/ScrollToTop";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -13,6 +15,23 @@ type PageContentProps = {
 };
 
 export const PageContent = ({ images, videos }: PageContentProps) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-[80vh]">
+        <div className="text-lg font-medium">Loading 请等一下...</div>
+      </div>
+    );
+  }
   return (
     <>
       <Tabs defaultValue="image" className="p-4">
@@ -21,7 +40,16 @@ export const PageContent = ({ images, videos }: PageContentProps) => {
           <TabsTrigger value="video">Video 视频</TabsTrigger>
         </TabsList>
         <TabsContent value="image">
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+          {loading && (
+            <div className="flex items-center justify-center h-[80vh]">
+              <div className="text-lg font-medium">Loading...</div>
+            </div>
+          )}
+          <div
+            className={`grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 ${
+              loading ? "opacity-0" : "opacity-100"
+            }`}
+          >
             {images.map((img) => (
               <ImageViewer key={img.id} publicId={img.id} alt={img.alt} />
             ))}
